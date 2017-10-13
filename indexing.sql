eeaee0849df8fa8f173fec9c866e01e7e3d1c997
Nathan N <Nitharios@users.noreply.github.com>
2017-10-13 11:22:50 -1000

    GOING FASTER!

---
+++ indexing.sql
@@ -0,0 +1,102 @@
+-- Create a new postgres user named `indexed_cars_user`
+-- CREATE USER indexed_cars_user;
+-- Create a new database named `indexed_cars` owned by `indexed_cars_user`
+-- \c indexed_cars_user;
+-- DROP DATABASE IF EXISTS indexed_cars;
+-- CREATE DATABASE indexed_cars OWNER = indexed_cars_user;
+-- Run the provided `scripts/car_models.sql` script on the `indexed_cars` database
+-- \c indexed_cars;
+-- \i scripts/car_models.sql;
+-- -- Run the provided `scripts/car_model_data.sql` script on the `indexed_cars` database **10 times** 
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql;
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql;
+-- \i scripts/car_model_data.sql;
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql; 
+-- \i scripts/car_model_data.sql; 
+  --  _there should be **223380** rows in `car_models`_
+
+-- Run a query to get a list of all `make_title` values from the `car_models` table where the `make_code` is `'LAM'`, without any duplicate rows, and note the time somewhere. (should have 1 result)
+-- SELECT DISTINCT make_title
+--   FROM car_models
+--   WHERE make_code LIKE '%LAM%';
+-- 27.824ms
+-- Run a query to list all `model_title` values where the `make_code` is `'NISSAN'`, and the `model_code` is `'GT-R'` without any duplicate rows, and note the time somewhere. (should have 1 result)
+-- SELECT DISTINCT model_title
+--   FROM car_models
+--   WHERE make_code = 'NISSAN'
+--   AND model_code = 'GT-R';
+-- 25.549ms
+-- Run a query to list all `make_code`, `model_code`, `model_title`, and year from `car_models` where the `make_code` is `'LAM'`, and note the time somewhere. (should have 1360 rows)
+-- SELECT make_code, 
+--   model_code, 
+--   model_title, 
+--   year
+--   FROM car_models
+--   WHERE make_code = 'LAM';
+-- Run a query to list all fields from all `car_models` in years between `2010` and `2015`, and note the time somewhere (should have 78840 rows)
+-- SELECT *
+--   FROM car_models
+--   WHERE year BETWEEN 2010 and 2015;
+-- 71.997ms
+-- Run a query to list all fields from all `car_models` in the year of `2010`, and note the time somewhere (should have 13140 rows)
+-- SELECT *
+--   FROM car_models
+--   WHERE year = '2010';
+-- 37.346ms
+-- Given the current query requirements, "should get all make_titles", "should get a list of all model_titles by the make_code", etc.  
+-- Create indexes on the columns that would improve query performance.
+
+-- To add an index:
+
+-- ```
+-- CREATE INDEX [index name]
+--   ON [table name] ([column name(s) index]);
+-- ```
+
+-- Record your index statements in `indexing.sql`
+
+-- Write the following statements in `indexing.sql`
+-- Create a query to get a list of all `make_title` values from the `car_models` table where the `make_code` is `'LAM'`, without any duplicate rows, and note the time somewhere. (should have 1 result)
+-- CREATE INDEX
+--   ON car_models(make_title);
+-- SELECT DISTINCT make_title
+--   FROM car_models
+--   WHERE make_code LIKE '%LAM%';
+-- 24.831ms 
+-- Create a query to list all `model_title` values where the `make_code` is `'NISSAN'`, and the `model_code` is `'GT-R'` without any duplicate rows, and note the time somewhere. (should have 1 result)
+-- SELECT DISTINCT model_title
+--   FROM car_models
+--   WHERE make_code = 'NISSAN'
+--   AND model_code = 'GT-R';
+-- 21.511 ms
+-- Create a query to list all `make_code`, `model_code`, `model_title`, and year from `car_models` where the `make_code` is `'LAM'`, and note the time somewhere. (should have 1360 rows)
+-- CREATE INDEX
+--   ON car_models(make_code);
+-- SELECT make_code, 
+--        model_code,
+--        model_title, 
+--        year
+--   FROM car_models
+--   WHERE make_code LIKE '%LAM%'
+-- 46.235ms
+-- 29.739ms AFTER INDEX
+-- Create a query to list all fields from all `car_models` in years between `2010` and `2015`, and note the time somewhere (should have 78840 rows)
+-- CREATE INDEX
+--   ON car_models(year);
+-- SELECT *
+--   FROM car_models
+--   WHERE year 
+--   BETWEEN 2010 and 2015;
+-- 73.663ms
+-- 53.042ms AFTER INDEX
+-- Create a query to list all fields from all `car_models` in the year of `2010`, and note the time somewhere (should have 13140 rows)
+-- SELECT *
+--   FROM car_models
+--   WHERE year = '2010';
+-- 11.787ms
+
